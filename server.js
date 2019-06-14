826086ab22cc86899ba1fb87cf01280fc60c55ea
Nica Zulueta <nicacassandrazulueta@gmail.com>
2019-06-13 21:04:25 -0700

    added form

--- server.js
+++ server.js
@@ -2,26 +2,34 @@ const http = require('http')
 const fs = require('fs')
 const qs = require('querystring')
 
-let names = [];
-fs.readFile('./guests.txt', (err, data) => {
-  names = (data.toString().split('|'))
-  console.log(names)
-})
+let names = []
 
-const server = http.createServer((res, req) => {
+
+const server = http.createServer((req, res) => {
+
   if (req.url === '/') {
-  if (req.method === 'GET') {
-  res.write(`<html><head></head><body><h1>Guest Book</h1>
-  <ul>${names.map(name => `<li>${name}</li>`)}</ul>
+    if (req.method === 'GET') {
+      fs.readFile('./guests.txt', (err, data) => {
+        if (err) { console.log(error) }
+        names = data.toString().split('|')
+      })
+      res.write(`<html><head></head><body><h1>Guest Book</h1>
+  <ul>${names.map(name => `<li>${name}</li>`).join('')}</ul>
+  <form method='POST'><input name='name' type='text' /><button>Add Name</button></form>
   </body></html>`)
-  res.end();
-  } else {
-    res.write('hello')
-  }
+      res.end();
+    } else {
+      const data = []
+      req.on('data', (chunk) => {data.push(chunk)});
+      req.on('end', () => {
+        const body = qs.parse(data.toString())
+        names.push(body.name)
+        console.log(names)
+      })
+    }
   }
 }
 )
-
 server.listen(3000)
 
 // const http = require('http');
